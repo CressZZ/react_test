55305f27d5528af0a5c69b7c4b1814cba94b7a05
yjpark0228 <yjpark0228@ncsoft.com>
2024-04-03 17:20:14 +0900

    init setting

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from '@/scss/App.scss';
+import img from '@/img/img1.webp';
 
 export default function App() {
-  console.log(styles.locals);
+  console.log(styles);
   return (
     <div>
-      <h1 className={`${styles.test}`}>Welcome to my app!!</h1>
+      <h1 id={`${styles.h1}`}>Welcome to my app!!</h1>
       <div className={styles.test2}></div>
+      <img src={img} alt="" />
     </div>
   );
 }
